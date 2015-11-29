7b52fe5b13d0f5429a79fc99d609fbe74b21e38f
Uku Pattak <uku.pattak@transferwise.com>
2015-11-29 13:05:57 +0200

    Remove unit test stuff, too heavy weight for the project

--- src/History.js
+++ src/History.js
@@ -2,21 +2,27 @@
 
 export default class History {
 
-    search(callback, historyLimit) {
-        chrome.history.search({text: 'http://southpark.cc.com/full-episodes/'}, function (results) {
-            var seen = [];
-            var found;
+    constructor(limit) {
+        this.limit = limit;
+    }
 
-            for (var i = 0; i < results.length; i ++) {
-                if (results[i].lastVisitTime < historyLimit)
-                    continue;
+    search(callback) {
+        chrome.history.search({text: 'http://southpark.cc.com/full-episodes/'}, this.collectFrom);
+    }
 
-                found = results[i].url.match(/s\d+e\d+/);
+    collectFrom(results) {
+        var seen = [];
+        var found;
 
-                if (found)
-                    seen.push({ season: found[0].match(/\d+/g)[0], episode: found[0].match(/\d+/g)[1] });
-            }
-            callback(seen);
-        });
+        for (var i = 0; i < results.length; i ++) {
+            if (results[i].lastVisitTime < this.limit)
+                continue;
+
+            found = results[i].url.match(/s\d+e\d+/);
+
+            if (found)
+                seen.push({ season: found[0].match(/\d+/g)[0], episode: found[0].match(/\d+/g)[1] });
+        }
+        callback(seen);
     }
 }
--- test/unit/History.spec.js
+++
@@ -1,9 +0,0 @@
-import History from '../src/History';
-
-describe('History service spec', () => {
-    'use strict';
-
-    it('is ture', () => {
-        expect(true).toBeTruthy();
-    });
-});
