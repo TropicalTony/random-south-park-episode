abde6a6efb6c07d1f174b06d11ab60c6cbc629df
syyfilis <tonistenno92@gmail.com>
2015-12-03 19:59:24 +0200

    save btn funconality added

--- ui/script.js
+++ ui/script.js
@@ -16,21 +16,26 @@ function buildSeasonList(season){
 function buildEpisodeList(season){
 	var div = document.getElementById("episodeList");
 	var currentSeason = JSON.parse(localStorage.getItem('SouthParkSeason'+ season.toString()) );
-	console.log("Current season: " + season.toString());
-	console.log(currentSeason);
+	//console.log("Current season: " + season.toString());
+	//console.log(currentSeason);
+	var episodeNames =  JSON.parse(localStorage.getItem('EpisodeNames' + season.toString()) );
 	for (i = 0; i < JSON.parse(localStorage.getItem('seasonLengths'))[season-1]; i++){
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.value = "Episode" + (i+1).toString();
-		checkbox.checked = true;
+		checkbox.id = i+1;
+		
 		if(currentSeason.indexOf(i+1) == -1){
+			checkbox.checked = true;
+		}
+		else{
 			checkbox.checked = false;
 		}
 		div.appendChild(checkbox);
 
 		    var label = document.createElement('label');
             label.htmlFor = "Episode" + (i+1).toString();
-            label.appendChild(document.createTextNode("Episode " + (i+1).toString()));
+            label.appendChild(document.createTextNode( episodeNames[i]));
 
             div.appendChild(label);
             div.appendChild(document.createElement("br"));   
@@ -44,4 +49,15 @@ buildSeasonList();
 document.getElementById("seasonList").onchange = function(){
 	document.getElementById("episodeList").innerHTML = "";
 	buildEpisodeList(document.getElementById("seasonList").value);
+};
+
+document.getElementById("saveButton").onclick = function(){
+	var selectedSeason = document.getElementById("seasonList").value;
+	var newEpisodes = []
+	for (i = 0; i < JSON.parse(localStorage.getItem('seasonLengths'))[selectedSeason-1]; i++){
+		if(!document.getElementById((i+1).toString()).checked){
+			newEpisodes.push(i+1);
+		}
+	}
+	localStorage.setItem("SouthParkSeason" + selectedSeason.toString() ,JSON.stringify(newEpisodes));
 };
