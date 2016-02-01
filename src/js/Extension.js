dd3d21c5ad8f4463a1ebe491cfba11db58ae9695
Uku Pattak <ukupat@gmail.com>
2016-02-01 19:53:39 +0200

    Send out Event to Google Analytics when User is shown a Episode

--- src/js/Extension.js
+++ src/js/Extension.js
@@ -6,10 +6,22 @@ import {hasToInitSeriesInfo, hasToUpdateSeriesInfo, initSeriesInfo, updateSeries
 export class Extension {
 
     constructor() {
+        this.initGoogleAnalytics();
+
         if (hasToInitSeriesInfo())
             initSeriesInfo();
 
         this.addListener();
+    }
+
+    initGoogleAnalytics() {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-71620285-2', 'auto');
+        ga('send', 'pageview');
     }
 
     addListener() {
@@ -37,6 +49,8 @@ export class Extension {
 
         markAsWatched(random.season, random.episode);
         this.openEpisode(url);
+
+        ga('send', 'event', 'Usage', 'Show episode', 'S' + random.season + 'E' + random.episode);
     }
 
     openEpisode(url) {
