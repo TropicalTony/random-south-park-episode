75c9472f57279eb1e162ff534d4972f428b20492
Uku Pattak <uku.pattak@zeroturnaround.com>
2016-08-05 22:11:11 +0300

    Setup database

---
+++ src/database.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    databaseURL: 'https://shining-inferno-2925.firebaseio.com'
+});
+
+const db = firebase.database();
+
+db.ref('/').on('value', function(snapshot) {
+    console.log(snapshot.val());
+});
